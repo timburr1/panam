b016f97e8a4e13515ab3df77430266111bbaab9a
Tim <tim.r.burr@gmail.com>
2020-01-09 13:16:48 -0700

    pure React, form at least looks like a form

--- src/App.js
+++ src/App.js
@@ -3,19 +3,27 @@ import ReactDOM from "react-dom";
 import { Router, Link } from "@reach/router";
 //import Form from "./Form";
 
-const Form = () => {    
+const Form = ({destination, isRoundTrip, departDate, passengerName, passengerBirthday, numPassengers, numBags}) => {    
     
   return React.createElement("div", {}, [
-      React.createElement("form", {}, ["Form goes here."]),
+      React.createElement("form", {}, [
+        React.createElement("h2", {}, "Destination: " + destination),
+        React.createElement("h2", {}, "Round Trip? " + isRoundTrip),
+        React.createElement("h2", {}, "Departure Date: " + departDate),
+        React.createElement("h2", {}, "Passenger Name: " + passengerName),
+        React.createElement("h2", {}, "Birthday: " + passengerBirthday),
+        React.createElement("h2", {}, "Passengers: " + numPassengers),
+        React.createElement("h2", {}, "Bags: " + numBags),
+      ])
   ])
 };
 
 const App = () => {
 
-  //TODO: Router element not working properly
   return React.createElement("div", {}, [
     React.createElement("header", {}, "¡Bienvenidos a Aerolínea PanAmerica!"),
-    React.createElement(Form)
+    React.createElement(Form, {destination: "Madrid, Espana", isRoundTrip: true, departDate: new Date(), returnDate: new Date(),
+    passengerName: "Tu Nombre", passengerGender: 'm', passengerBirthday: new Date(1, 1, 2000), numPassengers: 1, numBags: 1}, )
   ]); 
 };
 
