81f708c163c81a22bf172cbe23b1c3f8554613e9
Tim <tim.r.burr@gmail.com>
2020-01-09 15:14:02 -0700

    destination dropdown working

--- src/App.js
+++ src/App.js
@@ -7,17 +7,7 @@ const App = () => {
   return (
     <div>
       <header>¡Bienvenidos a Aerolínea PanAmerica!</header>
-      <Form
-        destination="Madrid, Espana"
-        isRoundTrip="true"
-        departDate={new Date()}
-        returnDate={new Date()}
-        passengerName="Tu Nombre"
-        passengerGender="m"
-        passengerBirthday={new Date(2000, 0, 1)}
-        numPassengers="1"
-        numBags="1"
-      />
+      <Form />
     </div>
   );
 };
--- src/Form.js
+++ src/Form.js
@@ -1,36 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const Form = ({
-  destination,
-  isRoundTrip,
-  departDate,
-  passengerName,
-  passengerBirthday,
-  numPassengers,
-  numBags
-}) => {
-  //   return React.createElement("div", {}, [
-  //     React.createElement("form", {}, [
-  //       React.createElement("h2", {}, "Destination: " + destination),
-  //       React.createElement("h2", {}, "Round Trip? " + isRoundTrip),
-  //       React.createElement("h2", {}, "Departure Date: " + departDate),
-  //       React.createElement("h2", {}, "Passenger Name: " + passengerName),
-  //       React.createElement("h2", {}, "Birthday: " + passengerBirthday),
-  //       React.createElement("h2", {}, "Passengers: " + numPassengers),
-  //       React.createElement("h2", {}, "Bags: " + numBags)
-  //     ])
-  //   ]);
+export const Form = ({}) => {
+  const [destination, setDestination] = useState("Madrid, Espana (MAD)");
+  const [isRoundTrip, setRoundTrip] = useState(true);
+  const [departDate, setDepartDate] = useState(new Date());
+  const [returnDate, setReturnDate] = useState(new Date());
+  const [passengerName, setPassengerName] = useState("Tu Nombre");
+  const [passengerBirthday, setPassengerBirthday] = useState(
+    new Date(2020, 0, 1)
+  );
+  const [numPassengers, setNumPassengers] = useState(1);
+  const [numBags, setNumBags] = useState(1);
 
   return (
-    <div>
+    <div className="form">
       <form>
-        <h2>Destino: {destination}</h2>
-        <h2>Fecha de Ida: {departDate.toString()}</h2>
-        <h2>Ida y Vuelta? {isRoundTrip}</h2>
-        <h2>Nombre de Pasajero: {passengerName}</h2>
-        <h2>Fecha de Nacimiento: {passengerBirthday.toString()}</h2>
-        <h2>Pasajeros: {numPassengers}</h2>
-        <h2>Maletas: {numBags}</h2>
+        <label htmlFor="destination">
+          <h2>
+            Destino:
+            <select
+              id="destination"
+              value={destination}
+              onChange={event => setDestination(event.target.value)}
+              onBlur={event => setDestination(event.target.value)}
+            >
+              <option>Arendelle (FRZN)</option>
+              <option>Beunos Aires, Argentina (EZE)</option>
+              <option>Cuidad Crevase, Alderaan (ALD)</option>
+              <option>Habana, Cuba (HAV)</option>
+              <option>Madrid, Espana (MAD)</option>
+              <option>Mazatlan, Mexico (MZT)</option>
+              <option>Meridian, Idaho, USA (KLN)</option>
+            </select>
+          </h2>
+        </label>
+        <label htmlFor="departDate">
+          <h2>
+            Fecha de Ida:
+            <input
+              id="departDate"
+              value={departDate.toString()}
+              type="date"
+              onChange={event => setDepartDate(event.target.value)}
+            />
+          </h2>
+        </label>
+        <label htmlFor="isRoundTrip">
+          <h2>Tipo de Viaje: Solo Ida o Ida y Vuelta?</h2>
+        </label>
+        <label htmlFor="passengerName">
+          <h2>
+            Nombre de Pasajero:
+            <input
+              id="passengerName"
+              value={passengerName}
+              type="text"
+              onChange={event => setPassengerName(event.target.value)}
+            />
+          </h2>
+        </label>
+        <label>
+          <h2>
+            Fecha de Nacimiento:
+            <input
+              id="passengerBirthday"
+              value={passengerBirthday.toString()}
+              type="date"
+              onChange={event => setPassengerBirthday(event.target.value)}
+            />
+          </h2>
+        </label>
+        <label htmlFor="numPassengers">
+          <h2>
+            Pasajeros:
+            <input
+              id="numPassengers"
+              value={numPassengers}
+              type="number"
+              onChange={event => setNumPassengers(event.target.value)}
+            />
+          </h2>
+        </label>
+        <label htmlFor="numBags">
+          <h2>
+            Maletas:
+            <input
+              od="numBags"
+              value={numBags}
+              type="number"
+              onChange={event => setNumBags(event.target.value)}
+            />
+          </h2>
+        </label>
+        <button>Reset</button>
+        <button>Submit</button>
       </form>
     </div>
   );
