120b985497ac4d0ecee284311b1a3f18c162075e
Tim <tim.r.burr@gmail.com>
2020-01-09 08:29:39 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { Router, Link } from "@reach/router";
+import Form from "./Form";
 
 const App = () => {
-  //const { destination, isRoundTrip, departDate, returnDate, passengerName, passengerGender, numPassengers, numBags } = trip;
 
-  const [destination, updatedDestination] = useState("Meridian, ID");
-  const isRoundTrip = useState(true);
-  const passengerName = useState("Fake Nombre");
-  const passengerBirthday = useState(new Date(1, 1, 2000));
-  const numPassengers = useState(1);
-  const numBags = useState(1);
-
+  //TODO: Router element not working properly
   return (
-    <div>      
-      <h1>¡Bienvenidos a Aerolínea PanAmerica!</h1> 
-        
-      <form>        
-        Destino: <input type="text" value={destination} /><br /><br />
-        Solo Ida o Ida y Vuelta?<br /><br />
-        DEPART DATE; RETURN DATE <br />< br />
-        ¿Cual es tu apellido? <input type="text" value={passengerName} /><br /><br />
-        Fecha de nacimiento <input type="date" value={passengerBirthday} /><br/><br/> 
-        ¿Cuántos pasajeros? <input type="number" value={numPassengers} /><br /><br />
-        ¿Cuántas maletas? <input type="number" value={numBags} /><br/><br/>
-        <button>Submit</button>
-      </form>  
+    <div>
+      <header>
+        <Link to="/">¡Bienvenidos a Aerolínea PanAmerica!</Link>
+      </header> 
+      <Router>  
+        <Form path="/" />
+      </Router> 
     </div>
   );
 };
---
+++ src/Form.js
@@ -0,0 +1,42 @@
+const Form = () => {    
+    
+    return(
+        <div>
+            <form>Form goes here</form>
+        </div>
+    );
+};
+
+export default Form;
+
+/*<form onSubmit={this._handleSubmit}>        
+        Destino: {<input type="text" value={destination} /><br /><br /> 
+        <div className="radio">
+          <label>
+            <input 
+              type="radio" 
+              name="isRoundTrip" 
+              value="true"
+              checked={isRoundTrip === true}
+              onChange={this._handleRadio} />
+            Ida y Vuelta
+          </label>
+       </div>
+       <div className="radio">
+         <label>
+           <input 
+             type="radio" 
+             name="isRoundTrip" 
+             value="false"
+             checked={isRoundTrip === false}
+             onChange={this._handleRadio} />
+           Solo Ida
+         </label>
+    </div> 
+        DEPART DATE; RETURN DATE <br />< br />
+        ¿Cual es tu apellido? <input type="text" value={passengerName} /><br /><br />
+        Fecha de nacimiento <input type="date" value={passengerBirthday} /><br/><br/> 
+        ¿Cuántos pasajeros? <input type="number" value={numPassengers} /><br /><br />
+    ¿Cuántas maletas? <input type="number" value={numBags} /><br/><br/> 
+        <button type="submit">Submit</button>
+      </form>*/
